0c6e0ba26889e8d60523545656414bd6866d054b
muxa1508 <muxa1508@inbox.ru>
2022-10-03 17:57:05 +0300

    Added eraser method

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,23 @@ public class Main {
 
         list.add(new Person("Алексей", "Иванов Нечаев", 40));
         list.add(new Person("Иван", "Николаев ", 30));
-        list.add(new Person("Николай", "Петров сын Иосифа", 30));
+        list.add(new Person("Николай", "Петров сын Иосифа", 16));
         list.add(new Person("Петр", "Максимов", 50));
         list.add(new Person("Максим", "Алексеевич, рожденный под счастливой звездой", 11));
 
         ComparatorForPeople comp = new ComparatorForPeople(2);
 
         System.out.println("Оригинальный список: " + list);
+
+
+//        Predicate<Person> predicate = new Predicate<Person>() {
+//            @Override
+//            public boolean test(Person person) {
+//                return person.getAge() < 18;
+//            }
+//        };
+
+        list.removeIf(person -> person.getAge() < 18);
 
         Collections.sort(list, comp);
 
